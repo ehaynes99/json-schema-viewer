186c42da479d32d9c70c373e01bf33959a4dab94
Eric Haynes <ehaynes@better.com>
2021-07-26 11:14:57 -0400

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.scss'
+
+const App = () => (
+  <div className="application-container">
+    If there were actual application content, it would go here...
+  </div>
+)
+
+export default App
